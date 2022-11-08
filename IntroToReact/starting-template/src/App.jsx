e7e9c338c7fe39577e8d6d9c312a12616eeef721
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-11-08 11:32:16 -0500

    feat: first commit

---
+++ IntroToReact/starting-template/src/App.jsx
@@ -0,0 +1,22 @@
+// importing react and other files
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </main>
+  )
+}
+
+// Arrow Function Syntax 
+// const App = () => {
+//   return(
+//     <main>
+//       React ⚛️ + Vite ⚡ + Replit 🌀
+//     </main>
+//   )
+// }
+
+export default App
+
---
+++ IntroToReact/starting-template/src/index.jsx
@@ -0,0 +1,12 @@
+// Boilerplate code to ensure we are importing REACT
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+
+// GET ELEMENT BY ID - just ensure we can access the HTML Element
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
